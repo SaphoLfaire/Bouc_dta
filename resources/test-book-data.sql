1d2f6ef549a00f8b1a2b16c6e811eda787e7899e
saupetit <saupetit@MTP-D320-004.DIGINAMIC.LOCAL>
2017-12-18 17:17:28 +0100

    Creation d'une mini fausse bdd de book

--- resources/test-book-data.sql
+++ resources/test-book-data.sql
@@ -1,2 +1,2 @@
-insert into user_(id, title, author, nb_page, date_publication, isbn) values (1, 'La flemme un art de vivre', 'Jesus', '1', '2017-05-02', '14562584521');
-insert into user_(id, title, author, nb_page, date_publication, isbn) values (2, 'La flemme un art de vivre tome 2', 'Jesus', '2', '2017-05-02', '4587456321585');
+insert into book(id, title, author, nb_pages, publication_date, isbn) values (68464861, 'La flemme un art de vivre', 'Jesus', '1', '2017-05-02', '14562584521');
+insert into book(id, title, author, nb_pages, publication_date, isbn) values (98787488, 'La flemme un art de vivre tome 2', 'Jesus', '2', '2017-05-02', '4587456321585');
