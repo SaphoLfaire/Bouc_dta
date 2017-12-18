f549935b31de8cbc1fde9218e86e332be1970781
saupetit <saupetit@MTP-D320-004.DIGINAMIC.LOCAL>
2017-12-18 15:02:46 +0100

    clean code

--- src/main/java/fr/dta/controller/MainController.java
+++ src/main/java/fr/dta/controller/MainController.java
@@ -16,34 +16,32 @@ import fr.dta.services.LibraryService;
 @RestController
 @RequestMapping(path = "/books")
 public class MainController {
-	
+
 	@Autowired
-	 LibraryService ls;
-	
-	
+	LibraryService ls;
+
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Book> getAll() {
 		return ls.getAll();
-		
 	}
-	
-	@RequestMapping (value="{id}",method = RequestMethod.GET )
+
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	public Book getById(@PathVariable int id) {
 		return ls.getById(id);
 	}
-	
-	@RequestMapping (method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public int createBook(@RequestBody Book book) {
 		ls.addBook(book);
 		return book.getId();
 	}
-	
-	@RequestMapping (value="{id}",method = RequestMethod.DELETE )
-	public int deleteBook (@PathVariable int id) {
+
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public int deleteBook(@PathVariable int id) {
 		ls.removeBook(id);
 		return id;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public int updateBook(@RequestBody Book book) {
 		ls.updateBook(book);
--- src/main/java/fr/dta/services/LibraryService.java
+++ src/main/java/fr/dta/services/LibraryService.java
@@ -11,49 +11,51 @@ import fr.dta.model.Book;
 
 @Service
 public class LibraryService {
-	
+
 	@Autowired
 	private BookRepository br;
 
-	public static LibraryService instance=null;
-	public static LibraryService getInstance(){
-		if(instance!=null) return instance;
-		else instance = new LibraryService();
+	public static LibraryService instance = null;
+
+	public static LibraryService getInstance() {
+		if (instance != null)
+			return instance;
+		else
+			instance = new LibraryService();
 		return instance;
 	}
-	
+
 	private ArrayList<Book> list = new ArrayList<Book>();
-	
-	public boolean addBook(Book b){
+
+	public boolean addBook(Book b) {
 		return br.add(b);
 	}
-	
-	public boolean removeBook(int id){
+
+	public boolean removeBook(int id) {
 		return br.delete(id);
 	}
-	
-	public ArrayList<Book> search(String criteria){
+
+	public ArrayList<Book> search(String criteria) {
 		ArrayList<Book> result = new ArrayList<Book>();
-		//for(Book b: list)if(b.matches(criteria.toLowerCase()))result.add(b);
+		// for(Book b: list)if(b.matches(criteria.toLowerCase()))result.add(b);
 		return result;
 	}
-	
-	
-	public boolean contains(int id){
-		return getById(id)!=null;
+
+	public boolean contains(int id) {
+		return getById(id) != null;
 	}
-	
-	public Book getById(int id){
+
+	public Book getById(int id) {
 		return br.getById(id);
-		
+
 	}
-	
-	public List<Book> getAll(){
+
+	public List<Book> getAll() {
 		return br.getAll();
 	}
-	
+
 	public Boolean updateBook(Book book) {
 		return br.update(book);
 	}
-	
+
 }
